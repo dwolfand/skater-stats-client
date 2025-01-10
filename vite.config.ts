8fe1c50870679cba3ffd2ccd6b10c177637d1117
David Wolfand <david@bellsant.com>
2025-01-10 08:48:38 -0500

    Add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,47 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: "/",
-  server: {
-    port: 3000,
-    open: true,
-    cors: true,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        404: "./public/404.html",
+export default defineConfig(({ mode }) => {
+  // Use different GA IDs based on mode
+  console.log("mode", mode);
+  const gaId = mode === "production" ? "G-VPFZ5HFC98" : "G-DEVELOPMENT";
+
+  // Only include GA script if ID is provided
+  const gaScript = gaId
+    ? `
+    <script async src="https://www.googletagmanager.com/gtag/js?id=${gaId}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${gaId}');
+    </script>
+  `
+    : "";
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: "html-transform",
+        transformIndexHtml(html) {
+          return html.replace("%ENV_GOOGLE_ANALYTICS%", gaScript);
+        },
+      },
+    ],
+    base: "/",
+    server: {
+      port: 3000,
+      open: true,
+      cors: true,
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+          404: "./public/404.html",
+        },
       },
     },
-  },
-}));
+  };
+});
