15a8f6e48c9d568faa5c2381ed1e77f29eeaebdc
David Wolfand <david@bellsant.com>
2025-01-12 22:04:39 -0500

    Make it a pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -29,6 +30,35 @@ export default defineConfig(({ mode }) => {
           return html.replace("%ENV_GOOGLE_ANALYTICS%", gaScript);
         },
       },
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: ["favicon.ico", "logo192.png", "logo512.png"],
+        manifest: {
+          name: "Skater Stats",
+          short_name: "Skater Stats",
+          description: "Figure Skating Statistics and Results",
+          theme_color: "#2B6CB0",
+          background_color: "#ffffff",
+          display: "standalone",
+          icons: [
+            {
+              src: "favicon.ico",
+              sizes: "64x64 32x32 24x24 16x16",
+              type: "image/x-icon",
+            },
+            {
+              src: "logo192.png",
+              type: "image/png",
+              sizes: "192x192",
+            },
+            {
+              src: "logo512.png",
+              type: "image/png",
+              sizes: "512x512",
+            },
+          ],
+        },
+      }),
     ],
     base: "/",
     server: {
