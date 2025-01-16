e187af70040d572fafb59424eac6298b9d0a662a
David Wolfand <david@bellsant.com>
2025-01-16 14:41:57 -0500

    removing unused file

--- src/components/Competition.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export function Competition() {
-  const { year, ijsId } = useParams<{ year: string; ijsId: string }>();
-
-  // ... rest of the component code using year/ijsId for fetching
-}
